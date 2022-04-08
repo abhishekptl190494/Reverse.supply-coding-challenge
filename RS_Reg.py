18cf6fb27c2f819eaad041d892e384136cacd301
abhishekptl190494 <81931862+abhishekptl190494@users.noreply.github.com>
2022-04-09 00:42:15 +0200

    Update RS_Reg.py

--- RS_Reg.py
+++ RS_Reg.py
@@ -1,42 +1,48 @@
+"""
+Code > reverse.supply coding challenge
+Authour > Abhishek Patel
+Date > 07.04.2022
+Location > Am Teltowkanal 7, 14513 Teltow 
+
+"""
+
 import json
 import re
 
-
 class Address:
+    """
+     Return > Working with different address formats and their elements
+
+    """
 
     def __init__(self,address):
-        self.address=address
+         """
+         :type address: String
 
-    def street(self):
+         """
+         self.address=address
 
-        return re.findall("\D+[A-z]+",self.address)[0]
-
-    def houseNumber(self):
-
-        return re.findall("[0-9]+[a-z]",self.address)[0]
-
-    def postcode(self):
-
-        return re.findall("\d{5}",self.address)[0]
-
-    def cityname(self):
-
-        return re.findall("\D+[A-z]+",self.address)[1]
+    def parse(self):
+        print(self.address)
+        parsed = re.findall(r'(\D*)(\w{2})(,?)(\s*)(\d{5})(\s*)(\D*)', self.address)
+        print(parsed)
+        return {
+            "street": parsed[0][0],
+            "housenumber": parsed[0][1],
+            "postalcode": parsed[0][4],
+            "city": parsed[0][6]
+        }
 
     
 if __name__=='__main__':
-
+    # Add address dictionary
     AddressDict = {}
 
-    AddressObject= Address('Am teltowkanal 7, 14513 teltow')
+    AddressObject= Address('Süthen 16 29482 Küsten') # insert address
 
-    AddressDict['street'] = AddressObject.street()
-    AddressDict['houseNumber'] = AddressObject.houseNumber()
-    AddressDict['postcode'] = AddressObject.postcode()
-    AddressDict['city'] = AddressObject.cityname()
+    json_object = json.dumps(AddressObject.parse(), ensure_ascii=False,indent=4)
+ 
+ # writing to output json
 
-    print(AddressDict)
-    json_object = json.dumps(AddressDict, indent=4)
-
-with open ("output.json", "w") as outfile:
-    outfile.write (json_object)
+    with open ("output.json", "w", encoding='utf8') as outfile:
+        outfile.write (json_object)
